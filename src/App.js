d2f48e743b21ea835ca22535e102d17f90cf3e21
mayukha91 <sumasindhu@outlook.com>
2021-02-13 23:33:37 -0700

    Added HTML
    installed bootstrap, added search button image from font awesome and added some content in html page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <form>
+        <input type="search" placeholder="Enter a city" />
+        <button class="search">
+          <i class="fas fa-search"></i>
+        </button>
+      </form>
+      <h1>New York </h1>
+      <ul>
+        <li>
+          <div class="row ">
+            <div class="col-6 day">January 15</div>
+            <div class="col-6 time"> 08:00</div>
+          </div>
+        </li>
+        <li>
+          <div class="row">
+            <div class="col-6 temp">
+              <strong> 56</strong>
+              <small>
+                <a href="#" id="cen">
+                  °C
+                </a>{" "}
+                |
+                <a href="#" id="fah">
+                  °F
+                </a>
+              </small>
+            </div>
+            <div class="col-6 imgDesc">
+              <div class="row img1">
+                <img
+                  src="http://openweathermap.org/img/wn/10d@2x.png"
+                  alt=""
+                  id="icon"
+                />
+              </div>
+              <div class="row sub1">Cloudy</div>
+            </div>
+          </div>
+        </li>
+      </ul>
+      <small>
+        <a href="https://github.com/mayukha91/Vanilla-weather-app">
+          open-source{" "}
         </a>
-      </header>
+        by Suma
+      </small>
     </div>
   );
 }
-
-export default App;
